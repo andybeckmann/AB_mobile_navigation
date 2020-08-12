e6bef22a1dbe2fbbc52d4bb1870ef6cc08f87916
Andy Beckmann <abec19@gmail.com>
2020-08-11 20:50:38 -0400

    Add support for a third level of navigation

--- js/build/global.js
+++ js/build/global.js
@@ -8,6 +8,7 @@
 
 function mobileNavigationToggle(navId) {
 
+	// Menu toggle
 	var toggleButton = document.getElementById(navId);
 
 	toggleButton.onclick = function() {
@@ -15,4 +16,13 @@ function mobileNavigationToggle(navId) {
 		document.getElementById('main-navigation').classList.toggle('open');
 	};
 
+	// Sub navigation toggle
+	var subNavToggle = document.querySelectorAll('.main-navigation-parent-item a');
+
+	for (var i = 0; i < subNavToggle.length; i++) {
+
+		subNavToggle[i].onclick = function () {
+			this.parentNode.classList.toggle('open');
+		};
+	}
 }
